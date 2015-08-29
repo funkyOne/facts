434e0c243b682c0561b1a86141003fadbaa5bc63
lega <oleg.novikdichko@gmail.com>
2015-08-29 20:13:35 +0300

    added dev server restart

---
+++ server/gulpfile.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let gulp = require('gulp');
+let nodemon = require('gulp-nodemon');
+
+gulp.task('default', function () {
+    nodemon({
+        script: 'bin/www',
+        ext: 'js',
+        ignore: ['node_modules/*']
+    }).on('restart', function () {
+        console.log('restarted!');
+    });
+});
