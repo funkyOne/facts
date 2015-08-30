887b484693226bf12d37d245b4f8933a1fa727c7
lega <oleg.novikdichko@gmail.com>
2015-08-30 22:23:36 +0300

    reimplemented facts endpoint with koa-route

--- server/src/routes/facts.js
+++ server/src/routes/facts.js
@@ -1,154 +1,48 @@
 'use strict';
 
-let express = require('express');
-let router = express.Router();
-let MongoClient = require('mongodb').MongoClient;
-let _ = require('lodash');
-let storeIssue = require('../import').storeIssue;
+const route = require('koa-route');
+const parse = require('co-body');
+const thunkify = require('thunkify');
+const thenifyAll = require('thenify-all');
+const _ = require('lodash');
+const db = require('../db');
 
-router.get('/', function (req, res) {
-    let db = req.app.get('db');
-    db.fact.find({}, function (err, facts) {
-        err && console.error(err);
+const run = thunkify(db.run).bind(db);
 
-        db.category.find({}, function (err, categories) {
-            err && console.error(err);
+let Fact = thenifyAll(db.fact, {}, ['find', 'save']);
+let Category = thenifyAll(db.category, {}, ['find']);
+let FactCategory = thenifyAll(db.fact_category, {}, ['find']);
 
-            db.fact_category.find({}, function (err, fact_category) {
-                err && console.error(err);
+function factsRouter(app) {
 
-                let factsIndexed = _.indexBy(facts, f=>f.id);
-                let cat_facts = _.groupBy(fact_category, c=> c.category_id);
+    app.use(route.get('/facts', function*() {
+        let facts = yield Fact.find({});
+        let categories = yield Category.find({});
+        let factCategory = yield FactCategory.find({});
 
-                _.forEach(categories, function (cat) {
-                    cat.facts = _.map(cat_facts[cat.id], (function (fc) {
-                        return factsIndexed[fc.fact_id];
-                    }));
-                });
+        let factsIndexed = _.indexBy(facts, f=>f.id);
+        let cat_facts = _.groupBy(factCategory, c=> c.category_id);
 
-                res.send(categories);
-            });
+        _.forEach(categories, function (cat) {
+            cat.facts = _.map(cat_facts[cat.id], fc => factsIndexed[fc.fact_id]);
         });
-    });
-});
 
-router.put('/:id', (req, res) => {
-    if (!req.body) {
-        res.sendStatus(400);
-    }
+        this.body = categories;
+    }));
 
-    let db = req.app.get('db');
-    let factId = req.params.id;
-    let fact = _.extend({id: factId}, req.body);
+    app.use(route.get('/facts/:id/issues', function*(id) {
+        let issues = yield run('select i.* from issue i join fact_issue fi on i.id=fi.issue_id and fi.fact_id=$1', [id]);
 
-    db.fact.save(fact, (err, updated)=> {
-        if (err) {
-            res.status(500).send(err);
-            return;
-        }
+        this.body = issues;
+    }));
 
-        res.sendStatus(200);
-    });
-});
+    app.use(route.put('/facts/:id', function*(id) {
+        const save = thunkify(db.fact.save).bind(db.fact);
+        const fact = yield parse(this);
+        yield save(fact);
 
-router.get('/:id/issues', function (req, res) {
-    let db = req.app.get('db');
-    let factId = req.params.id;
-
-    db.run('select i.* from issue i join fact_issue fi on i.id=fi.issue_id and fi.fact_id=$1', [factId], function (err, issues) {
-        if (err) {
-            res.status(500).send(err);
-            return;
-        }
-
-        res.send(issues);
-    });
-
-    //db.fact_issue.find({fact_id: req.params.id}, (err, fact_issues) => {
-    //    if (err) {
-    //        res.send(err);
-    //        return;
-    //    }
-    //
-    //    MongoClient.connect('mongodb://localhost:27017/exampleDb', (err, db) => {
-    //        if (err) {
-    //            res.send(err);
-    //            return;
-    //        }
-    //
-    //        let issueIds = _(fact_issues).pluck('issue_id').map(String).value();
-    //
-    //        db.collection('issues').find({id: {$in: issueIds}})
-    //            .toArray(function (err, issues) {
-    //
-    //                if (err) {
-    //                    res.send('err')
-    //                }
-    //
-    //                res.send(issues)
-    //            })
-    //    })
-    //});
-});
-
-
-function map(db) {
-    return new Promise(function (resolve, reject) {
-        let collection = db.collection('issues');
-
-        collection.aggregate([{
-                $match: {
-                    'fields.customfield_10006': {
-                        $exists: true,
-                        $ne: null
-                    }
-                }
-            }, {$project: {'epic': '$fields.customfield_10006', 'key': 1, 'html': '$fields.description'}}])
-            .toArray(function (err, issues) {
-
-                let promises = [];
-                if (err) {
-                    reject(err);
-                    return;
-                }
-
-                let grouped = _.groupBy(issues, 'epic');
-
-                let categories = [];
-                _.forEach(grouped, function (group, name) {
-                    if (!name)
-                        return;
-
-                    console.log('epic - ' + name);
-
-
-                    let cat = {facts: group};
-
-                    let epic = _.findWhere(issues, {key: name});
-
-                    //epic doesn't exist
-                    if (epic) {
-                        cat.title = epic.summary;
-                        categories.push(cat);
-                    }
-                    else {
-                        let promise = storeIssue(db, name).then(function (issue) {
-                            cat.title = issue.fields.summary;
-                            categories.push(cat);
-                        });
-
-                        promises.push(promise);
-                    }
-                });
-
-
-                Promise.all(promises).then(function () {
-                    //resolve();
-                    resolve(categories);
-                });
-            });
-    });
+        this.status = 200;
+    }));
 }
 
-
-module.exports = router;
+module.exports = factsRouter;
---
+++ server/src/routes/facts_old.js
@@ -0,0 +1,154 @@
+'use strict';
+
+let express = require('express');
+let router = express.Router();
+let MongoClient = require('mongodb').MongoClient;
+let _ = require('lodash');
+let storeIssue = require('../import').storeIssue;
+
+router.get('/', function (req, res) {
+    let db = req.app.get('db');
+    db.fact.find({}, function (err, facts) {
+        err && console.error(err);
+
+        db.category.find({}, function (err, categories) {
+            err && console.error(err);
+
+            db.fact_category.find({}, function (err, fact_category) {
+                err && console.error(err);
+
+                let factsIndexed = _.indexBy(facts, f=>f.id);
+                let cat_facts = _.groupBy(fact_category, c=> c.category_id);
+
+                _.forEach(categories, function (cat) {
+                    cat.facts = _.map(cat_facts[cat.id], (function (fc) {
+                        return factsIndexed[fc.fact_id];
+                    }));
+                });
+
+                res.send(categories);
+            });
+        });
+    });
+});
+
+router.put('/:id', (req, res) => {
+    if (!req.body) {
+        res.sendStatus(400);
+    }
+
+    let db = req.app.get('db');
+    let factId = req.params.id;
+    let fact = _.extend({id: factId}, req.body);
+
+    db.fact.save(fact, (err, updated)=> {
+        if (err) {
+            res.status(500).send(err);
+            return;
+        }
+
+        res.sendStatus(200);
+    });
+});
+
+router.get('/:id/issues', function (req, res) {
+    let db = req.app.get('db');
+    let factId = req.params.id;
+
+    db.run('select i.* from issue i join fact_issue fi on i.id=fi.issue_id and fi.fact_id=$1', [factId], function (err, issues) {
+        if (err) {
+            res.status(500).send(err);
+            return;
+        }
+
+        res.send(issues);
+    });
+
+    //db.fact_issue.find({fact_id: req.params.id}, (err, fact_issues) => {
+    //    if (err) {
+    //        res.send(err);
+    //        return;
+    //    }
+    //
+    //    MongoClient.connect('mongodb://localhost:27017/exampleDb', (err, db) => {
+    //        if (err) {
+    //            res.send(err);
+    //            return;
+    //        }
+    //
+    //        let issueIds = _(fact_issues).pluck('issue_id').map(String).value();
+    //
+    //        db.collection('issues').find({id: {$in: issueIds}})
+    //            .toArray(function (err, issues) {
+    //
+    //                if (err) {
+    //                    res.send('err')
+    //                }
+    //
+    //                res.send(issues)
+    //            })
+    //    })
+    //});
+});
+
+
+function map(db) {
+    return new Promise(function (resolve, reject) {
+        let collection = db.collection('issues');
+
+        collection.aggregate([{
+                $match: {
+                    'fields.customfield_10006': {
+                        $exists: true,
+                        $ne: null
+                    }
+                }
+            }, {$project: {'epic': '$fields.customfield_10006', 'key': 1, 'html': '$fields.description'}}])
+            .toArray(function (err, issues) {
+
+                let promises = [];
+                if (err) {
+                    reject(err);
+                    return;
+                }
+
+                let grouped = _.groupBy(issues, 'epic');
+
+                let categories = [];
+                _.forEach(grouped, function (group, name) {
+                    if (!name)
+                        return;
+
+                    console.log('epic - ' + name);
+
+
+                    let cat = {facts: group};
+
+                    let epic = _.findWhere(issues, {key: name});
+
+                    //epic doesn't exist
+                    if (epic) {
+                        cat.title = epic.summary;
+                        categories.push(cat);
+                    }
+                    else {
+                        let promise = storeIssue(db, name).then(function (issue) {
+                            cat.title = issue.fields.summary;
+                            categories.push(cat);
+                        });
+
+                        promises.push(promise);
+                    }
+                });
+
+
+                Promise.all(promises).then(function () {
+                    //resolve();
+                    resolve(categories);
+                });
+            });
+    });
+}
+
+
+module.exports = router;
