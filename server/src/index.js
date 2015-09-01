709c26b5812cca459eb670e48a0b65b15f83084d
lega <oleg.novikdichko@gmail.com>
2015-09-01 21:28:47 +0300

    renamed koa to index anc cleaned up

---
+++ server/src/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+//to initialize __dirname
+require('./base-init');
+
+const logger = require('koa-logger');
+const serve = require('koa-static');
+const mount = require('koa-mount');
+const koa = require('koa');
+const path = require('path');
+const factRoutes = require('./routes/facts');
+const categoryRoutes = require('./routes/categories');
+
+const app = koa();
+const clientProjectRoot = path.join(__dirname, '../..', 'client');
+
+app.use(logger());
+app.use(serve(path.join(clientProjectRoot, 'dist')));
+app.use(mount('/bower_components', serve(path.join(clientProjectRoot, 'bower_components'))));
+
+factRoutes(app);
+categoryRoutes(app);
+
+app.listen(3000, function () {
+    console.log('Listening on port', 3000);
+});
